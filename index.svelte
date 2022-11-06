a508f4a4f9acfe711f868065977c08d77d974073
Leva <levbulychou1234567890@outlook.com>
2022-11-06 18:46:43 +0300

    Create index.svelte

---
+++ index.svelte
@@ -0,0 +1,16 @@
+<script>
+  import {config} from "@LevaCompany/icons-core"
+
+  import "@style.css"
+  import "style.less"
+  import "@LevaCompany/icons-svelte/style.css"
+  import "@LevaCompany/icons-svelte/style.less"
+  confingStyle.addLess = "style.less"
+  confingStyle.addCss = "style.css"
+</script>
+
+<div>
+  <p>
+    <LevaIcon icon={ThumbsUp}> Hello Svelte!
+  </p>
+</div>
